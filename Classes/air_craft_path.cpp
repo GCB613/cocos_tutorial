6582e26985de6d249b49dd5854d900fed283913e
Garrett Bodily <garrett.bodily@mx.com>
2015-09-25 05:15:51 -0600

    Add new class for storing the path of the air craft.

---
+++ Classes/air_craft_path.cpp
@@ -0,0 +1,9 @@
+//
+//  air_craft_path.cpp
+//  AirTrafficController
+//
+//  Created by Garrett Bodily on 9/25/15.
+//
+//
+
+#include "air_craft_path.h"
---
+++ Classes/air_craft_path.h
@@ -0,0 +1,16 @@
+//
+//  air_craft_path.h
+//  AirTrafficController
+//
+//  Created by Garrett Bodily on 9/25/15.
+//
+//
+
+#pragma once
+
+#include <stdio.h>
+
+class AirCraftPath {
+public:
+  AirCraftPath() {}
+};
